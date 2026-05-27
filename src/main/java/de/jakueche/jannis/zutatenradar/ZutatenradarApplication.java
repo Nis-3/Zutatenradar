c5cd2b8867ac5ec9db72781a4965a313efb62472
Jannis <janniswweber@gmail.com>
2026-05-27 08:37:57 +0200

    Add Project

---
+++ src/main/java/de/jakueche/jannis/zutatenradar/ZutatenradarApplication.java
@@ -0,0 +1,13 @@
+package de.jakueche.jannis.zutatenradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZutatenradarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZutatenradarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/jakueche/jannis/zutatenradar/ZutatenradarApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jakueche.jannis.zutatenradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZutatenradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
